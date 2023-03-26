687c1aa09060666a41e509bfae8a62c36abe743b
StarProxima <maks.prozorov1@yandex.ru>
2023-03-26 15:47:06 +0300

    Disable DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MainApp extends StatelessWidget {
     final appTextStyles = AppTextStyles.light;
 
     return DevicePreview(
-      // enabled: false,
+      enabled: false,
       builder: (context) => ProviderScope(
         child: MaterialApp(
           locale: DevicePreview.locale(context),
