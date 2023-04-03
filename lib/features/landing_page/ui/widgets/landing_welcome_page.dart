ffd0c54e6a284ca7a4a3b4edb5df2a79dea72082
StarProxima <maks.prozorov1@yandex.ru>
2023-04-04 01:37:38 +0300

    Edit layout in landing welcome page

--- lib/features/landing_page/ui/widgets/landing_welcome_page.dart
+++ lib/features/landing_page/ui/widgets/landing_welcome_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cube_system/styles/app_theme_context_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,64 +11,114 @@ class LandingWelcomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(
-        parent: AlwaysScrollableScrollPhysics(),
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Stack(
           children: [
-            const SizedBox(height: 24),
-            FittedBox(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+            // Positioned(
+            //   top: 0,
+            //   left: 100,
+            //   child: Transform.translate(
+            //     offset: const Offset(0, -230),
+            //     child: Transform.rotate(
+            //       angle: 1.4,
+            //       child: Container(
+            //         height: 350,
+            //         width: 350,
+            //         decoration: const BoxDecoration(
+            //           color: Color.fromARGB(255, 151, 207, 255),
+            //           shape: BoxShape.circle,
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+
+            Transform.translate(
+              offset: const Offset(-0, -150),
+              child: SizedBox(
+                child: Assets.rive.shapes.rive(
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 130, sigmaY: 130),
+                child: const SizedBox(),
+              ),
+            ),
+
+            Positioned.fill(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 36),
+                child: SafeArea(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "КУБ",
-                        style: context.textStyles.largeTitle.copyWith(
-                          fontSize: 56,
-                          color: context.colors.primary,
+                      const SizedBox(height: 196),
+                      FittedBox(
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Assets.icons.cubeLogo.svg(
+                                height: 96,
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      "Куб",
+                                      style: context.textStyles.largeTitle
+                                          .copyWith(
+                                        fontSize: 36,
+                                        color: context.colors.text,
+                                      ),
+                                    ),
+                                    Text(
+                                      ".",
+                                      style: context.textStyles.largeTitle
+                                          .copyWith(
+                                        fontSize: 36,
+                                        color: context.colors.primary,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  "Расписание",
+                                  style: context.textStyles.largeTitle.copyWith(
+                                    fontSize: 36,
+                                    color: context.colors.text,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
+                      const SizedBox(height: 48),
                       Text(
-                        ".",
-                        style: context.textStyles.largeTitle.copyWith(
-                          fontSize: 56,
-                          color: context.colors.text,
+                        "Организуйте свою академическую жизнь c современным электронным расписанием: удобнылегкий доступ, приятный дизайн и заметки для студентов и преподавателей.",
+                        style: context.textStyles.subTitle.copyWith(
+                          fontSize: 16,
                         ),
                       ),
+                      const SizedBox(height: 32),
                     ],
                   ),
-                  Text(
-                    "Расписание",
-                    style: context.textStyles.largeTitle.copyWith(
-                      fontSize: 56,
-                      color: context.colors.primary,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-            const SizedBox(height: 24),
-            Text(
-              "Организуйте свою академическую жизнь c современным электронным расписанием: легкий доступ, приятный дизайн и заметки для студентов и преподавателей.",
-              style: context.textStyles.subTitle.copyWith(),
-            ),
-            const SizedBox(height: 64),
-            Center(
-              child: Assets.brooklyn.smartPeople2.svg(
-                height: 280,
-                width: 280,
-              ),
-            ),
-            const SizedBox(height: 32),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
